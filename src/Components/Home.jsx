46a1a172b7640c53a9ff3039bc78c44dc979f2a5
Bpmauge <briahanamauge@pursuit.org>
2020-03-14 23:06:04 -0400

    add skills icons

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -1,9 +1,21 @@
 import React from 'react'
+import { FaHtml5, FaCss3Alt, FaNode, FaReact, FaGitAlt } from 'react-icons/fa';
+import {IoLogoJavascript} from 'react-icons/io'
+import {DiPostgresql} from 'react-icons/di'
+
 const Home = () => {
     return (
         <>
             <h1>Home</h1>
-           </>
+             <FaHtml5 className = 'icon' />
+             <FaCss3Alt className = 'icon' />
+             <IoLogoJavascript className = 'icon' />
+             <FaNode className = 'icon' />
+             <FaReact className = 'icon' /> 
+             <FaGitAlt className = 'icon' />
+             <DiPostgresql className = 'icon' />
+
+        </>
     )
 }
 
