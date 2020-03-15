ec7556425c06948fc972acf349e387ceb14245a2
Bpmauge <briahanamauge@pursuit.org>
2020-03-15 10:51:10 -0400

    add css file

--- src/Components/Projects.jsx
+++ src/Components/Projects.jsx
@@ -1,10 +1,19 @@
 import React from 'react'
+import Pokemon from '../images/Pokemon.png';
+import '../CSS/Projects.css';
 const Projects = () => {
     return (
         <>
             <h1>Projects</h1>
-    </>
-    )
-}
-
-export default Projects;
+            <div class="card">
+                <img src= {Pokemon} alt="Avatar" />
+                    <div class="container">
+                        <h4><b>John Doe</b></h4>
+                        <p>Architect & Engineer</p>
+                    </div>
+</div>
+        </>
+            )
+        }
+        
+        export default Projects;
