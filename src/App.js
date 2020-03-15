2088862da01cace47836fad68ccc4bd038b2082e
Bpmauge <briahanamauge@pursuit.org>
2020-03-14 23:06:19 -0400

    add contact page

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { Link, Route, Switch } from 'react-router-dom';
 import Projects from './Components/Projects';
 import Home from './Components/Home';
 import About from './Components/About';
+import Contact from './Components/Contact';
 
 const App = () => {
   return (
@@ -11,12 +12,14 @@ const App = () => {
         <Link to="/">Home</Link> 
         <Link to="/Projects">Projects</Link>{' '}
         <Link to="/about">About</Link>{' '}
+        <Link to="/contact">Contact</Link>{' '}
       </header>
 
       <Switch>
         <Route exact path="/" component={Home} />
         <Route path="/Projects" component={Projects} />
         <Route path="/about" component={About} />
+        <Route path="/contact" component={Contact} />
       </Switch>
     </div>
   );
