aba4a2d7b4cfb4cb196e100eabe65cf7a6019a46
Bpmauge <briahanamauge@pursuit.org>
2020-03-14 22:31:31 -0400

    add home, about, project component

---
+++ src/Components/About.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+const About = () => {
+    return (
+        <>
+            <h1>About</h1>
+            <p>This YouTube clone was built by Briahana as part of her unit 4 assessment.</p>
+            <p>Technologies used for this project include Youtube's API, React, CSS and several npm packages, like 'react-youtube'. </p>
+            <p> On the home page, you can search for video and select your pick from the first 8 choices from your search. </p>
+            <p>Clicking on the video takes you to the video's page where you can watch and can leave comments. </p>
+        </>
+    )
+}
+
+export default About;
