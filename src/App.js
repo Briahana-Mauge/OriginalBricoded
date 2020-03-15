454f49cc1e2960beed255965b10fb5c74b76cb23
Bpmauge <briahanamauge@pursuit.org>
2020-03-14 22:32:05 -0400

    update files for routing

--- src/App.js
+++ src/App.js
@@ -1,26 +1,25 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Link, Route, Switch } from 'react-router-dom';
+import './styles.css';
+import Projects from './Components/Projects';
+import Home from './Components/Home';
+import About from './Components/About';
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className="app">
+      <header>
+        <Link to="/">Home</Link> 
+        <Link to="/Projects">Projects</Link>{' '}
+        <Link to="/about">About</Link>{' '}
       </header>
+
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route path="/Projects" component={Projects} />
+        <Route path="/about" component={About} />
+      </Switch>
     </div>
   );
-}
-
+};
 export default App;
