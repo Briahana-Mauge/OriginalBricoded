6b1a36454865a79c98dc1ef77a339597136bd33c
Bpmauge <briahanamauge@pursuit.org>
2020-03-16 22:09:52 -0400

    add NavBar component

--- src/App.js
+++ src/App.js
@@ -4,23 +4,21 @@ import Projects from './Components/Projects';
 import Home from './Components/Home';
 import About from './Components/About';
 import Contact from './Components/Contact';
+import Thread from './Components/Thread'
+import NavBar from './Components/NavBar';
 import './App.css';
 
 const App = () => {
   return (
     <div className="app">
-      <header>
-        <Link to="/">Home</Link> 
-        <Link to="/Projects">Projects</Link>{' '}
-        <Link to="/about">About</Link>{' '}
-        <Link to="/contact">Contact</Link>{' '}
-      </header>
+    <NavBar />
 
       <Switch>
         <Route exact path="/" component={Home} />
         <Route path="/Projects" component={Projects} />
         <Route path="/about" component={About} />
         <Route path="/contact" component={Contact} />
+        <Route path="/days-of-code" component={Thread} />
       </Switch>
     </div>
   );
