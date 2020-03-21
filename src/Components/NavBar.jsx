e03021f38dcc6ec4b02e6bfcc959c5efb30f413a
Bpmauge <briahanamauge@pursuit.org>
2020-03-20 21:23:14 -0400

    nav bar has toggle button

--- src/Components/NavBar.jsx
+++ src/Components/NavBar.jsx
@@ -1,16 +1,19 @@
 import React from 'react';
 import { Link } from "react-router-dom";
+import Button from './Button';
 import '../CSS/NavBar.css';
 
-const NavBar = () => {
+const NavBar = (props) => {
     return (
       <div className="app">
+        
         <header className = 'nav'>
           <Link to="/">Home</Link> {' '}
           <Link to="/Projects">Projects</Link>{' '}
           <Link to="/about">About</Link>{' '}
           <Link to="/contact">Contact</Link>{' '}
           <Link to="/days-of-code">Days Of Code</Link>{' '}
+          <Button handleSwitch = {props.handleSwitch}/>
         </header>
   
      
