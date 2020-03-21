743c1c46dd95eac87beadf6d3b2e2634bd8d140b
Bpmauge <briahanamauge@pursuit.org>
2020-03-21 12:17:07 -0400

    added second nav

---
+++ src/Components/NavBar2.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import { Link } from "react-router-dom";
+import Button from './Button';
+import '../CSS/NavBar.css';
+
+const NavBar = (props) => {
+    return (
+      <div className="nav">
+        
+        <header className = 'nav'>
+          <Link to="/">Home</Link> {' '}
+          <Link to="/Projects">Projects</Link>{' '}
+          <Link to="/contact">Contact</Link>{' '}
+          <Link to="/days-of-code">Days Of Code</Link>{' '}
+          <Button handleSwitch = {props.handleSwitch}/>
+        </header>
+  
+     
+      </div>
+    );
+  };
+  export default NavBar;
+  
