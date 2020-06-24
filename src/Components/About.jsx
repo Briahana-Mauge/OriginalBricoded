cbeeb2055cbb75091e0bd78a2fbd2a8e34b485e4
Bpmauge <briahanamauge@pursuit.org>
2020-06-24 19:49:48 -0400

    draft about section

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -1,13 +1,12 @@
 import React from 'react'
 const About = () => {
     return (
-        <>
-            <h1>About</h1>
-            <p>This YouTube clone was built by Briahana as part of her unit 4 assessment.</p>
-            <p>Technologies used for this project include Youtube's API, React, CSS and several npm packages, like 'react-youtube'. </p>
-            <p> On the home page, you can search for video and select your pick from the first 8 choices from your search. </p>
-            <p>Clicking on the video takes you to the video's page where you can watch and can leave comments. </p>
-        </>
+        <div className = 'about'>
+            <h1>Hi, I'm Briahana!</h1>
+            <p>I am a full-stack software engineer who enjoys working on cross functional teams. </p>
+            <p>When I'm not building with React, you can find me snuggled in a corner reading or listening to my favorite podcasts.
+            Keep scrolling to view some of my projects. I look forward to connecting with you.</p>
+        </div>
     )
 }
 
