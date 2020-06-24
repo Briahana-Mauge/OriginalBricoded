8daf743267fde172b2a1b71fe8b0fae8b5ac062f
Bpmauge <briahanamauge@pursuit.org>
2020-06-24 19:05:35 -0400

    fix hamburger menu to auto close

--- src/Components/Sidebar.jsx
+++ src/Components/Sidebar.jsx
@@ -3,29 +3,53 @@ import { Link } from "react-router-dom";
 import { slide as Menu } from "react-burger-menu";
 import '../CSS/Sidebar.css'
 import Button from './Button';
+import { connect } from 'react-redux';
 
-export default props => {
-  return (
-    <Menu className = 'menu'>
-      <Link className = 'menu-item' to = "/">Home</Link>
+class HamburgerMenu extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      menuOpen: false
+    }
+  }
 
-      <Link className="menu-item" to="/projects">Projects</Link>
+  handleStateChange(state) {
+    this.setState({ menuOpen: state.isOpen })
+  }
 
-      <Link className="menu-item" to="/about">
-        About
-      </Link>
+  closeMenu() {
+    this.setState({ menuOpen: false })
+  }
 
-      <Link className="menu-item" to="/contact">
-        Contact
-      </Link>
-      <Link className="menu-item" to="/days-of-code">
-        Days of Code
-      </Link>
-      {/* <Button handleSwitch = {props.handleSwitch} switched =  {true} /> */}
-      {/* <label className="switch" >
-                <input type="checkbox"  onChange= {props.handleSwitch} checked = {true}/>
-                <span className="slider round"></span>
-            </label> */}
-    </Menu>
-  );
-};
+  // This can be used to toggle the menu, e.g. when using a custom icon
+  // Tip: You probably want to hide either/both default icons if using a custom icon
+  // See https://github.com/negomi/react-burger-menu#custom-icons
+  toggleMenu() {
+    this.setState(state => ({ menuOpen: !state.menuOpen }))
+  }
+
+  render() {
+    return (
+      <div>
+        <Menu className = 'hamburger'
+          isOpen={this.state.menuOpen}
+          onStateChange={(state) => this.handleStateChange(state)}
+        >
+          <Link onClick={() => this.closeMenu()} className='menu-item' to="/">Home</Link>
+          <Link onClick={() => this.closeMenu()} className="menu-item" to="/about">About</Link>
+          <Link onClick={() => this.closeMenu()} className="menu-item" to="/contact">Contact</Link>
+          <Link onClick={() => this.closeMenu()} className="menu-item" to="/projects">Projects</Link>
+          <Link onClick={() => this.closeMenu()} className="menu-item" to="/days-of-code">Days of Code</Link>
+          <Button />
+        </Menu>
+      </div>
+    )
+  }
+}
+// export default HamburgerMenu
+const mapStateToProps = (state) => {
+  return state;
+
+}
+
+export default connect(mapStateToProps)(HamburgerMenu)
