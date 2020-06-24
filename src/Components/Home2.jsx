38e7c768ee5252e67b21831fe44690ab60dbd144
Bpmauge <briahanamauge@pursuit.org>
2020-06-24 19:44:17 -0400

    updated home2 page

--- src/Components/Home2.jsx
+++ src/Components/Home2.jsx
@@ -1,8 +1,5 @@
 import React from 'react'
 import '../CSS/Home2.css';
-import { FaHtml5, FaCss3Alt, FaNode, FaReact, FaGitAlt } from 'react-icons/fa';
-import { IoLogoJavascript } from 'react-icons/io'
-import { DiPostgresql } from 'react-icons/di'
 import logoYellow from '../logoYellow.svg';
 import Nav2 from './NavBar2';
 import Contact from './Contact';
