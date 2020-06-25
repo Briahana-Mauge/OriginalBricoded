ff837507586dd4af763fbed75fb9a6475cd71e55
Bpmauge <briahanamauge@pursuit.org>
2020-06-24 20:12:11 -0400

    redo the contact component

--- src/Components/Contact.jsx
+++ src/Components/Contact.jsx
@@ -1,17 +1,43 @@
 import React from 'react'
 import { FaGithub, FaLinkedinIn } from 'react-icons/fa';
-import {AiOutlineFilePdf, AiOutlineMail} from 'react-icons/ai'
+import { AiOutlineFilePdf, AiOutlineMail } from 'react-icons/ai'
 
 const Contact = () => {
     return (
-        <>
-            <h1>Contact</h1>
-             <FaGithub className = 'icon contact' />
-             <FaLinkedinIn className = 'icon contact' />
-             <AiOutlineFilePdf className = 'icon contact' />
-             <AiOutlineMail className = 'icon contact' />
+        <div className = 'Contact-icons'>
+            {/* <h1>Contact</h1> */}
 
-        </>
+            <a
+                target='_blank'
+                href='https://github.com/Briahana-Mauge'
+                className='icon contact'
+                rel='noopener noreferrer'>
+                <FaGithub className='icon contact' />
+            </a>
+
+            <a
+                target='_blank'
+                href='https://www.linkedin.com/in/bmauge/'
+                className='icon contact'
+                rel='noopener noreferrer'>
+                <FaLinkedinIn className='icon contact' />
+            </a>
+
+            <a
+                target='_blank'
+                href='https://docs.google.com/document/d/1xkq1KqpZ4ycZ17BtYJ6Jtu5AXyAslX9RXDLNcrjk7iM/edit?usp=sharing'
+                className='icon contact'
+                rel='noopener noreferrer'>
+                <AiOutlineFilePdf className='icon contact' />
+            </a>
+
+            <a
+                href='mailto:briahanamauge@pursuit.org'
+                className='icon contact'>
+                <AiOutlineMail className='icon contact' />
+            </a>
+
+        </div>
     )
 }
 
