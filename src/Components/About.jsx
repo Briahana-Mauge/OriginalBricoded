be137da9f84921eafb0929b5770bc7ffabc149ff
Bpmauge <briahanamauge@pursuit.org>
2020-06-26 12:03:37 -0400

    fix typo

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -5,7 +5,7 @@ const About = () => {
             <h1>Hi, I'm Briahana!</h1>
             <p>I am a full-stack software engineer who made a career change from education. </p>
             <p>When I'm not building projects, you can find me binging on netflix, listening to my favorite podcasts, or at a concert.
-            I am look forward to connecting with you.</p>
+            I am looking forward to connecting with you.</p>
         </div>
     )
 }
