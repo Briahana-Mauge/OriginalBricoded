99bb59036bd9d00579a9472e038ad6b0c7f419fc
Bpmauge <briahanamauge@pursuit.org>
2020-06-26 14:47:22 -0400

    add my whole name to the front page

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -2,10 +2,10 @@ import React from 'react'
 const About = () => {
     return (
         <div className = 'about'>
-            <h1>Hi, I'm Briahana!</h1>
-            <p>I am a full-stack software engineer who made a career change from education. </p>
-            <p>When I'm not building projects, you can find me binging on netflix, listening to my favorite podcasts, or at a concert.
-            I am looking forward to connecting with you.</p>
+            <h1>Hi, I'm Briahana Maugé!</h1>
+            <p>I am a math teacher by day, early-career full stack web developer by night. </p>
+            <p>When I'm not building projects, you can find me binging Netflix shows, listening to my favorite podcasts, or at a concert.
+            Check out my projects and I am looking forward to connecting with you!</p>
         </div>
     )
 }
