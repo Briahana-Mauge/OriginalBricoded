f900913a1ea91ecf5102994a8d8ff7038e64c3df
Bpmauge <briahanamauge@pursuit.org>
2020-08-05 00:12:01 -0400

    add alt tags to small crowns

--- src/Components/NavBar2.jsx
+++ src/Components/NavBar2.jsx
@@ -32,11 +32,9 @@ const NavBar = (props) => {
     <div className="nav">
       <header className='nav2'>
       
-        <Link to="/"><img className = 'crown' src = {crownYellow} ></img>Home</Link> {' '}
-        {/* <img className = 'crown' src = {crownYellow}></img> */}
-        <Link to="/Projects"><img className = 'crown' src = {crownYellow} ></img>Projects</Link>{' '}
-        {/* <img className = 'crown' src = {crownYellow}></img>         */}
-        <Link to="/days-of-code"><img className = 'crown' src = {crownYellow} ></img>Days Of Code</Link>{' '}
+        <Link to="/"><img className = 'crown' src = {crownYellow} alt = 'small crown in yellow hovering over "home" button'></img>Home</Link> {' '}
+        <Link to="/Projects"><img className = 'crown' src = {crownYellow} alt = 'small crown in yellow hovering over "projects" button'></img>Projects</Link>{' '}
+        <Link to="/days-of-code"><img className = 'crown' src = {crownYellow} alt = 'small crown in yellow hovering over "days of code" button'></img>Days Of Code</Link>{' '}
         {/* <Button /> */}
       </header>
 
