b37ec91924f3bec2d4a1ad4de4bcba2aaeaa03db
Bpmauge <briahanamauge@pursuit.org>
2020-09-03 00:12:02 -0400

    fix width of thread embbed and add 'back to top' button

--- src/Components/Thread.jsx
+++ src/Components/Thread.jsx
@@ -1,27 +1,46 @@
 import React from 'react'
 import { TwitterTimelineEmbed } from "react-twitter-embed";
+import BackToTop from "react-back-to-top-button";
+import '../CSS/Thread.css';
 // import Contact from './Contact';
 // import logoBlack from '../logoBlack.svg';
 import NavBar2 from './NavBar2';
+import { FaArrowUp } from 'react-icons/fa';
+
 
 
 const Thread = () => {
-    return (
-        <>
+    return ( 
+        <div className = 'thread'>
          <NavBar2  contact = 'other-pages' links = 'link'/>
             <>
             <div className='Tweets'>
             <br></br><br></br>
-                <div className="centerContent">
-                    <div className="selfCenter standardWidth">
+                <div className="centerContent" width= "85%">
+                    <div className="selfCenter standardWidth" width= "85%">
                         <TwitterTimelineEmbed sourceType="collection"
                             id="1205542982310596608"
-                            options={{ height: 1400, width: 400 }} />
+                            options={{ 
+                                height: "2400px", 
+                                width: "100%"
+                             }} 
+                            // transparent 
+                            // noHeader="true"
+        //   noBorders="true"
+        //   noFooter="true"
+                            />
                     </div>
                 </div>
             </div>
             </>
-        </>
+            <BackToTop /*showOnScrollUp*/
+        showAt={100}
+        speed={20}
+        easing="easeInOutQuint">
+        <span><FaArrowUp className="BackToTop" /></span>
+        </BackToTop>
+        </div>
+        
     )
 }
 
