7254a6e1afd5b2246e6e2e5b438c23dd6e92f6f2
Bpmauge <briahanamauge@pursuit.org>
2020-09-08 14:42:32 -0400

    add small black crown for pages other than main page

--- src/Components/NavBar2.jsx
+++ src/Components/NavBar2.jsx
@@ -3,45 +3,49 @@ import { Link } from "react-router-dom";
 // import Button from './Button';
 import '../CSS/NavBar.css';
 import { connect } from 'react-redux';
-import Contact from './Contact';
-import logoBlack from '../logoBlack.svg';
+// import Contact from './Contact';
+// import logoBlack from '../logoBlack.svg';
 import crownYellow from '../crown_yellow.PNG';
+import crownBlack from '../crown_black.PNG';
 const NavBar = (props) => {
-  if(props.contact){
-  return (
-    <div className="nav">
-      <div className = 'small_div'>
-        <img src={logoBlack} alt="logo in black" className='blackLogo' />
-        
-      <Contact className={`main ${props.contact}`} />
+  if (props.contact) {
+    return (
+      <div className="nav">
+        <header className= {`nav2 ${props.links}`}>
+
+          {/* <div className='new-nav'> */}
+          <Link to="/"><img className='crown' src={crownBlack} alt='small crown in B=black hovering over "home" button'></img><p>Home</p></Link> {' '}
+          <Link to="/Projects"><img className='crown' src={crownBlack} alt='small crown in black hovering over "projects" button'></img><p>Projects</p></Link>{' '}
+          <Link to="/days-of-code"><img className='crown' src={crownBlack} alt='small crown in black hovering over "days of code" button'></img><p>Days Of Code</p></Link>{' '}
+            {/* <Button /> */}
+          {/* </div> */}
+        </header>
+
+        {/* <div className='small_div'> */}
+          {/* <img src={logoBlack} alt="logo in black" className='blackLogo' /> */}
+
+          {/* <Contact className={`main ${props.contact}`} /> */}
+        {/* </div> */}
+
+
       </div>
-      <header className= /*nav2*/  {` ${props.links}`}>
-      
-        <div className = 'new-nav'>
-        <Link to="/">Home</Link> {' '}
-        <Link to="/Projects">Projects</Link>{' '}
-        <Link to="/days-of-code">Days Of Code</Link>{' '}
-        {/* <Button /> */}
-       </div>
-       </header> 
-       
-    </div>
-  );
-} else{
-  return (
-    <div className="nav">
-      <header className='nav2'>
-      
-        <Link to="/"><img className = 'crown' src = {crownYellow} alt = 'small crown in yellow hovering over "home" button'></img><p>Home</p></Link> {' '}
-        <Link to="/Projects"><img className = 'crown' src = {crownYellow} alt = 'small crown in yellow hovering over "projects" button'></img><p>Projects</p></Link>{' '}
-        <Link to="/days-of-code"><img className = 'crown' src = {crownYellow} alt = 'small crown in yellow hovering over "days of code" button'></img><p>Days Of Code</p></Link>{' '}
-        {/* <Button /> */}
-      </header>
+    );
+  } else {
+    return (
+
+      <div className="nav">
+        <header className='nav2 main-nav2'>
+
+          <Link to="/"><img className='crown' src={crownYellow} alt='small crown in yellow hovering over "home" button'></img><p>Home</p></Link> {' '}
+          <Link to="/Projects"><img className='crown' src={crownYellow} alt='small crown in yellow hovering over "projects" button'></img><p>Projects</p></Link>{' '}
+          <Link to="/days-of-code"><img className='crown' src={crownYellow} alt='small crown in yellow hovering over "days of code" button'></img><p>Days Of Code</p></Link>{' '}
+          {/* <Button /> */}
+        </header>
 
 
-    </div>
-  );
-}
+      </div>
+    );
+  }
 };
 // export default NavBar;
 const mapStateToProps = (state) => {
