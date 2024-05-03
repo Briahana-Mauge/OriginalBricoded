336a35289431655945fc0f69b8ae21d223ecfb3a
Briahana Maugé <52139668+Briahana-Mauge@users.noreply.github.com>
2024-05-03 03:08:34 -0400

    readd modules

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -1,11 +1,16 @@
 import React from 'react'
 const About = () => {
     return (
-        <div className = 'about'>
+        <div className='about'>
             <h1>Hi, I'm Briahana Maugé!</h1>
-            <p>I am a math teacher by day, early-career full stack web developer by night. </p>
-            <p>When I'm not building projects, you can find me binging Netflix shows, listening to my favorite podcasts, or at a concert.
-            Check out my projects and I am looking forward to connecting with you!</p>
+            <p>I'm a passionate and forward-thinking Software Engineer with a knack for turning ideas into impactful solutions. 
+                With expertise in full-stack development and a toolkit that includes JavaScript, Go, React.js, and PostgreSQL, I thrive on bringing innovation to life. 
+                What sets me apart is not just my technical prowess, but also my ability to connect with people and lead teams with empathy and enthusiasm. 
+                Whether it's crafting robust RESTful APIs or creating intuitive user interfaces, I approach every project with a human touch, ensuring that the end result not only meets technical standards but also resonates with users on a deeper level.
+                With a background in education, I understand the importance of clear communication and inclusive environments, making me a valuable asset to any team. 
+                Let's chat about how I can bring my passion and expertise to your organization. </p>
+            {/* <p>When I'm not building projects, you can find me binging Netflix shows, listening to my favorite podcasts, or at a concert.
+                Check out my projects and I am looking forward to connecting with you!</p> */}
         </div>
     )
 }
